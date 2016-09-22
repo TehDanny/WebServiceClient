1b60c56368ab2cc605214ae4be467d2f178e793c
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-09-22 13:09:46 +0200

    Completed People WCF client exercise

---
+++ WebServiceClient/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebServiceClient.PersonWcfService;
+
+namespace WebServiceClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            peopleGrid.AutoGenerateColumns = true;
+
+            PersonWcfServiceClient client = new PersonWcfServiceClient();
+            peopleGrid.ItemsSource = client.GetAllePersoner();
+        }
+    }
+}
